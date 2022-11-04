b710ce79056d7d80f0cdea0e831d3873762da745
Able <ablekilinda@gmail.com>
2022-11-04 04:56:30 +0300

    lotan

--- dest.js
+++ dest.js
@@ -1,6 +1,5 @@
 //arrray destructuring
-let name = "my name is able kilinda";
+let name = "my name is able kilinda is the person of a tech bro".split( " ");
 
-let a, b,c,d,e = name;
-
-console.log(a);
+let [,,,a,c,...d] = name;
+console.log(a,c,d);
